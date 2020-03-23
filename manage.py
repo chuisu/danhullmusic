fe82e2bf70958be4f68c51cfbff0f6ffc5e12e6f
chuisu <choi.brandon.h@gmail.com>
2020-03-23 13:38:20 -0400

    Reset django initialization while it still makes sense

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'danhullmusic.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dhmDjango.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
